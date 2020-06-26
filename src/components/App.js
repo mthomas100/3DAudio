3fce402503a97a4b4ce37a98af40fdd558c2e0f2
mthomas100 <matthewsamuelthomas@gmail.com>
2020-06-26 14:41:22 -0400

    single cube drop with collide detection

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,26 @@ import { Physics, usePlane, useBox } from 'use-cannon'
 import Plane from '../components/Plane'
 import Cube from '../components/Cube'
 import '../styles.css'
+import Tone from "tone";
 
 function App() {
+
+
+
+function handleCollide(e) {
+    console.log(e);
+}
 
   return (
     <Canvas shadowMap sRGB gl={{ alpha: false }} camera={{ position: [-1, 2, 5], fov: 50 }}>
     <color attach="background" args={['lightblue']} />
     <hemisphereLight intensity={0.35} />
     <spotLight position={[10, 10, 10]} angle={0.3} penumbra={1} intensity={2} castShadow />
-    <Physics>
-      <Plane />
-      <Cube />
-      <Cube position={[0, 10, -2]} />
-      <Cube position={[0, 20, -2]} />
+    <Physics gravity = {[0, -3, 0]}>
+      <Plane
+      onCollide={handleCollide}
+      />
+      <Cube position={[0, 5, -2]} />
     </Physics>
   </Canvas>
   )
--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -1,32 +1,13 @@
-import ReactDOM from 'react-dom'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
-import '../styles.css'
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { Physics, usePlane, useBox } from 'use-cannon';
 
-function Sphere(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
-  
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-  
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
-  
-    return (
-      <mesh
-        {...props}
-        ref={mesh}
-        scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-        onClick={e => setActive(!active)}
-        onPointerOver={e => setHover(true)}
-        onPointerOut={e => setHover(false)}>
-        <dodecahedronBufferGeometry attach="geometry" args={[1, 0]} />
-        <meshNormalMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-      
-    )
-  }
-
-  export default Sphere;
+export default function Cube(props) {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 0.2, 0.5], ...props }))
+  return (
+    <mesh receiveShadow castShadow ref={ref}>
+      <dodecahedronBufferGeometry attach="geometry" />
+      <meshNormalMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
+}
